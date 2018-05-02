3c3e9855d4dac9e03238f309447a40534511e596
Unknown <hemmerling@gmail.com>
2018-05-02 08:24:04 -0300

    fix

--- src/components/EspaciosMunicipales.vue
+++ src/components/EspaciosMunicipales.vue
@@ -11,7 +11,7 @@
 		<div class="row">
 			<paginate name="lugares" :list="lugares" :per="12" v-if="lugares.length>0" class="col-md-12 resultados">
 				<div v-show="!cargando" v-for="lugar in paginated('lugares')" :key="lugar.id" class="panel panel-default lugar">
-					<a :href="'/que-hacer/sitiosdeinteres/' + lugar.id + '/' + nombreToUrl(lugar.nombre)" >
+					<a :href="'/espacio/' + lugar.id + '/' + nombreToUrl(lugar.nombre)" >
 						<div class="lugar__foto panel-body" ><img v-if="lugar.fotos[0]" class="foto__img" :src="lugar.fotos[0].foto.original"/><img v-else class="foto__img" src="https://www.cordoba.gob.ar/wp-content/uploads/2017/10/catedral_de_crdoba_a_la_tarde.jpg"/></div>
 						<div class="panel-footer"><span class="lugar__nombre"><h4>{{lugar.nombre}}</h4><span class="lugar__direccion"><span class="fa fa-map-marker"></span>{{lugar.direccion}}</span></span></div>
 					</a></div>
