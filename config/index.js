3aae90fe014a572a53df234d46fe56ca36da1d5b
Unknown <hemmerling@gmail.com>
2018-08-01 10:14:22 -0300

    source

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/wp-content/uploads/proyectos/alojamientos-turismo/',
+    assetsPublicPath: '/wp-content/uploads/proyectos/oficinas-turismo/',
 
     /**
      * Source Maps
